8091dc42e00a54227701b26b278ab2350c8e4a7b
Josh <jkarbi3@uwo.ca>
2020-11-28 11:05:53 -0500

    Defining new game, join game, game update msg types

--- backend/main.py
+++ backend/main.py
@@ -4,16 +4,22 @@ main.py
 Create a websocket server.
 Listen for messages of types:
 - New game request: {"type": "new_game"}
+- Join game request: {"type": "join_game", "game_ID": 0}
 - Game updates: {"type": "update", "game_ID": "", "player_ID":"", "word_num": ""}
 
 Send back responses:
-- New game creation: {"type": "new_game", "created": "success", "game_ID": ""}
-- Game updates: {"type":"update", "game_ID": "", "status": "in_progress", "updates": [
-                            {"player_ID": 0, "progress": 0.50},
-                            {"player_ID": 1, "progress:: 0.66}
-                        ]  
-                }
-- Game finished: {"type: "update", "game_ID": "", "status": "finished", "winner_ID": 0}
+
+If we got a new game request:
+    - New game creation: {"type": "new_game", "created": "success", "game_ID": "", "paragraph": ""}
+If we got a join game request:
+    - Join game successful: {"type": "join_game", "game_ID": "", "paragraph": "" }
+If we got a game update:
+    - Game updates: {"type":"update", "game_ID": "", "status": "in_progress", "updates": [
+                                {"player_ID": 0, "progress": 0.50},
+                                {"player_ID": 1, "progress:: 0.66}
+                            ]  
+                    }
+    - Game finished: {"type: "update", "game_ID": "", "status": "finished", "winner_ID": 0}
 '''
 
 
