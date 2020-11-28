54440d2bb68c0422f151c0921df86cdc3aac3769
Trevor Smith <trevorsmith1317@gmail.com>
2020-11-28 15:58:14 -0500

    Update game.py

--- backend/game.py
+++ backend/game.py
@@ -1,84 +1,71 @@
 import json, random
 from multiprocessing import Queue
 
-
-
 class Game:
-    #queue, multiprocessing.queue obj #two - one I recieve from, one i send back
-    #queue.receive
 
     def __init__(self,qRecv, qSend):
+        # Set initial variables
         self.isGameDone = False
         self.winPlayerID = -1
         self.playerIDCount = -1
 
-        #set the queue object used to receive messages from server parent
+        # Set the queue object used to receive messages from server parent
         self.qRecv = qRecv
 
-        #set the queue object used to send messages from server parent
+        # Set the queue object used to send messages from server parent
         self.qSend = qSend
 
-        #Generate random paragraph
+        # Generate random paragraph
         with open('paragraph_bank.json') as paragraph_bank_file:
             paraBank = json.load(paragraph_bank_file)
             randIndex = random.randint(0,len(paraBank)-1)
 
-            #Store paragraph to this object
+            # Store paragraph to this object
             self.paragraph = paraBank[randIndex]
 
-        #Store length of paragraph to this object
+        # Store length of paragraph to this object
         self.paragraphLength = len(self.paragraph.split())
         
 
     def updatePlayerProgress(self, playerID, wordsCompleted):
-        #Calculate % of paragraph player has completed
+        # Calculate % of paragraph player has completed
         percentComplete = 100*(wordsCompleted/self.paragraphLength)
 
-        #If the player has completed the paragraph, they win.
+        # Round to nearest integer
+        percentComplete = round(percentComplete)
+
+        # If the player has completed the paragraph, they win.
         if percentComplete == 100:
             self.isGameDone = True
             self.winPlayerID = playerID
         
-        #Return the progress
+        # Return the progress
         return {"player_ID": playerID, "progress": percentComplete}
 
 
     def run(self):
+        # Continuous loop until the game finishes
         while not self.isGameDone:
 
+            # Recieve message from server parent
             message = self.qRecv.get()
 
+            # Execute joinGame() if that is the message type
             if message.get("type") == "join_game":
                 self.qSend.put(self.joinGame())
 
+            # Break this while loop if kill game is requested
             elif message.get("type") == "kill_game":
                 self.isGameDone = True
 
+            # Otherwise assume the message is a player's progress update
             else:
                 playerID = message.get("player_ID")
                 wordCount = message.get("word_num")
                 playerProgress = self.updatePlayerProgress(playerID, wordCount)
                 self.qSend.put(playerProgress)
 
-    
+    # Return a new player id and the game's paragraph
     def joinGame(self):
         self.playerIDCount += 1
         return {"player_ID": self.playerIDCount, "paragraph": self.paragraph}
-
-
-if __name__=="__main__":
-
-    #How to use
-
-    #create game object (switch send/recv queues in arguments)
-    game = Game(qSend, qRecv)
-
-    #join game returns paragraph and that everyone will be writing
-    game.joinGame()
-
-    #update a single player's progress by passing playerID and num of words completed (defined by split())
-    #returns playerID and their progress (%) in format: {"playerID": playerID, "progress": progress}
-    playersProgress = game.updatePlayerProgress(1, 10)
-
-    #isGameDone & winPlayerID to check if game has finished
-    isGameFinished = game.isGameDone
